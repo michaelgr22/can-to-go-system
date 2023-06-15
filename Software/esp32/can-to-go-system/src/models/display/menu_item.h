481755cf05fdef1acc9fb8bcbf19a06d2899041d
Michael Graml <michael.graml@st.oth-regensburg.de>
2023-06-15 09:09:37 +0200

    added model for menu item

---
+++ Software/esp32/can-to-go-system/src/models/display/menu_item.h
@@ -0,0 +1,16 @@
+#ifndef MENU_ITEM_H
+#define MENU_ITEM_H
+
+/*========== Includes =======================================================*/
+
+#include <stdio.h>
+
+/*========== Model Declaration =========================================*/
+struct MenuItem
+{
+    u_int8_t id;
+    u_int8_t is_selected;
+    char text[50];
+};
+
+#endif
